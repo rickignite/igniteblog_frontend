26f483a8dfd015ddeffd167e02d801c373f98500
Ricardo Lima <ricardo.lxique@gmail.com>
2021-09-27 15:12:57 -0500

    adjust URL ApolloClient

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -8,8 +8,18 @@ import {
 } from "@apollo/client";
 
 const cache = new InMemoryCache();
+const customFetch = (uri, options) => {
+  return fetch(uri, options)
+  .then(response => {
+    if (response.status >= 500) {  // or handle 400 errors
+      return Promise.reject(response.status);
+    }
+    return response;
+  });
+};
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+  fetch: customFetch
 });
 const client = new ApolloClient({
   cache,
